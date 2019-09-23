6984a2f0b6f83ea6a8548dd790248ea2ac9d6308
cwoodwarduk <calum.james.woodward@gmail.com>
2019-09-23 14:56:42 +0100

    doc blocks added to DB connect function

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * Function connects to the bike database
+ *
+ * @return PDO returns the database contents
+ *
+ */
 function dbConn(){
     $db = new PDO('mysql:host=db; dbname=calumsCollection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);

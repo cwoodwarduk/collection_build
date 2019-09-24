30ae5d002434f5a42dea2b5a6106c02c9c2c3bc4
cwoodwarduk <calum.james.woodward@gmail.com>
2019-09-24 10:00:02 +0100

    task 3 reviewed and done. Task 3 and 4 combined on this task branch.

--- functions.php
+++ functions.php
@@ -19,11 +19,33 @@ function dbConn(){
  *
  * @return array the data for each bike
  */
-function getManufacturer($db){
+function getData($db) :array {
     $query = $db->prepare("SELECT `manufacturer`, `model`, `wheelset`, `groupset` FROM `myBikes`;");
     $query->execute();
     $result = $query->fetchAll();
     return $result;
+}
+
+/*
+ * Takes data from getData output array and displays as HTML to be called into index.php
+ *
+ * @param $data array of the bikes data selected from getData
+ *
+ * @return a string of HTML code to be inserted to index.php to display on front end
+ */
+function getBike(array $data) :string
+{
+    $output = '';
+    foreach ($data as $bike) {
+        $output .=
+            '<div class="tableContent">' .
+            '<div class = "manufacturer"><p>' . $bike['manufacturer'] . '</p>' . '</div>' .
+            '<div class = "model"><p>' . $bike['model'] . '</p>' . '</div>' .
+            '<div class = "wheelset"><p>' . $bike['wheelset'] . '</p>' . '</div>' .
+            '<div class = "groupset"><p>' . $bike['groupset'] . '</p>' . '</div>' .
+            '</div>';
+    }
+    return $output;
 }
 
 ?>

ba55bd84f0c129f71464e921844396c0e66019af
cwoodwarduk <calum.james.woodward@gmail.com>
2019-09-25 15:44:24 +0100

    Form validation and sanitisation done, along with insertion into database in a single function. Tests to be written.

--- upload.php
+++ upload.php
@@ -1,1 +1,15 @@
 <?php
+require_once 'functions.php';
+
+$connection = dbConn();
+$state = addDataToDb($_POST, $connection);
+
+if ($state) {
+    header("Location: index.php");
+} else {
+    echo 'Incorrect input';
+    echo '<div class="buttonContainer">
+                <a href="formPage.php"><button type="button">GO BACK TO FORM</button></a>
+          </div>';
+}
+?>

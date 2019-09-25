95f3633b0b51c313295313eab82f01b84ed405e5
cwoodwarduk <calum.james.woodward@gmail.com>
2019-09-25 09:56:42 +0100

    moved procedural code in index.php to top of doc.

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  *
  */
 function dbConn() :PDO{
-    $db = new PDO('mysql:host=db; dbname=calumsCollection', 'root', 'password');
+    $db = new PDO('mysql:host=db; dbname=calumCollection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
